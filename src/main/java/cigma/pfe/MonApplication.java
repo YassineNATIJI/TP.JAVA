b16c8220b432963e27975b1de843ec07a73cfc73
☢☠YaS☫2iNe☠ ☢ <natiji.yassine@gmail.com>
2022-02-25 14:16:37 +0100

    mon.tp6

---
+++ src/main/java/cigma/pfe/MonApplication.java
@@ -0,0 +1,21 @@
+package cigma.pfe;
+    import cigma.pfe.models.Client;
+    import cigma.pfe.services.ClientService;
+    import cigma.pfe.controllers.ClientController;
+    import cigma.pfe.repositories.ClientRepository;
+    import org.springframework.context.ApplicationContext;
+    import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MonApplication {
+        public static void main(String[] args) {
+            ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+            ClientController ctrl = (ClientController) context.getBean("controller");
+            /*
+            ClientRepository ctrl0 = (ClientRepository) context.getBean("repository");
+            ClientService ctrl1 = (ClientService) context.getBean("service");
+            */
+            Client client = new Client(7,"Yassine");
+            ctrl.save(client);
+
+        }
+}
