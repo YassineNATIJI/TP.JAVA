8f32441c940cda826c414b485c412d28a2fadcbd
☢☠YaS☫2iNe☠ ☢ <natiji.yassine@gmail.com>
2022-03-18 04:46:23 +0100

    mon.tp13

---
+++ src/test/java/cigma/pfe/FrontendTp13ApplicationTests.java
@@ -0,0 +1,13 @@
+package cigma.pfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendTp13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
