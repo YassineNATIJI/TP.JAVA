7476ded1faf990e385140fcac9982ecfe320e0fc
☢☠YaS☫2iNe☠ ☢ <natiji.yassine@gmail.com>
2022-03-19 03:52:57 +0100

    mon.tp14

--- src/main/java/cigma/pfe/FrontendTp13Application.java
+++ src/main/java/cigma/pfe/FrontendTp13Application.java
@@ -1,9 +1,17 @@
 package cigma.pfe;
 
+import io.jsonwebtoken.Claims;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+
+import static cigma.pfe.config.security.JWTDemo.createJWT;
+import static cigma.pfe.config.security.JWTDemo.decodeJWT;
+
 
 @SpringBootApplication
 public class FrontendTp13Application {
@@ -16,4 +24,10 @@ public class FrontendTp13Application {
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 }
